6f40e1ac2369d988211080573257bfec1d9b8f40
emilano <82947413+aemilano@users.noreply.github.com>
2023-12-02 16:28:14 +0530

    First commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,50 @@
+public abstract class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int count;
+
+    public Product(int id, String name, double price, int count) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return !obj.getClass().equals(this.getClass());
+    }
+}
